338b31813cf285677abdbee22c50a3d2299b0d48
Michael Umeh <lazarus@Michaels-MacBook-Air.local>
2024-08-06 00:09:15 -0400

    practicing, not acutal project material

---
+++ src/main/java/UTD/demo/Users/UsersConfig.java
@@ -0,0 +1,7 @@
+package UTD.demo.Users;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UsersConfig {
+}
---
+++ src/main/java/UTD/demo/Users/UsersService.java
@@ -0,0 +1,19 @@
+package UTD.demo.Users;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<Users> getUsers(){
+        return usersRepository.findAll();
+    }
+}
