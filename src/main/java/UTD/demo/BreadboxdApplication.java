2dc8dd0627bf6f169a64ee41970e7a92cb763442
Michael Umeh <lazarus@Michaels-MacBook-Air.local>
2024-08-10 00:19:57 -0400

    spring initializer

---
+++ src/main/java/UTD/demo/BreadboxdApplication.java
@@ -0,0 +1,13 @@
+package UTD.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadboxdApplication.class, args);
+	}
+
+}
