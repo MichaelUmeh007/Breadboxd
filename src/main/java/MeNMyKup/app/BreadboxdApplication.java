fcc610251efae1f87d70ef24695c72294b05050e
Michael Umeh <lazarus@Michaels-MacBook-Air.local>
2024-08-10 00:54:13 -0400

    new backend template

---
+++ src/main/java/MeNMyKup/app/BreadboxdApplication.java
@@ -0,0 +1,13 @@
+package MeNMyKup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadboxdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MeNMyKup/app/BreadboxdApplicationTests.java
@@ -0,0 +1,13 @@
+package MeNMyKup.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadboxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
