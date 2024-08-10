5fe76c6e921d70957db718cc3084abeae840423c
Michael Umeh <74936187+MichaelUmeh007@users.noreply.github.com>
2024-08-10 00:05:02 -0400

    Delete src directory

--- src/main/java/UTD/demo/Users/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package UTD.demo.Users;
-
-import UTD.demo.Users.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
