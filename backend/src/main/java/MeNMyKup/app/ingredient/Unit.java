24108bd904fae7a8cc0fa39dc08ef73109d5e90d
Michael Umeh <lazarus@Michaels-MacBook-Air.local>
2024-08-21 17:52:54 -0400

    frontend refactor

---
+++ backend/src/main/java/MeNMyKup/app/ingredient/Unit.java
@@ -0,0 +1,34 @@
+package MeNMyKup.app.ingredient;
+
+public enum Unit {
+
+    // VOLUME
+
+    CUP,
+    ML,
+    L,
+    TSP,
+    TBSP,
+    FLOZ,
+    PT,
+    QT,
+    GL,
+
+    // WEIGHT
+
+    MG,
+    G,
+    KG,
+    LB,
+    OZ,
+
+    // LENGTH
+    MM,
+    CM,
+    M,
+    IN,
+    FT
+
+
+
+}
